37b44bf81a944be85a84abae0faa9e87b490fa0c
Oliver Rietmann <rioliver@student.ethz.ch>
2020-10-17 17:10:37 +0200

    Default properties

--- core/properties.py
+++ core/properties.py
@@ -1,5 +1,5 @@
 class Properties:
-    def __init__(self, color, ambient, diffuse, reflection=0.0):
+    def __init__(self, color, ambient=1.0, diffuse=0.0, reflection=0.0):
         self.color = color
         self.ambient = ambient
         self.diffuse = diffuse
