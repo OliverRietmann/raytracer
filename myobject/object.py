f331c9d09981bfd16b4b9956b43c44f597ad0957
Oliver Rietmann <rioliver@student.ethz.ch>
2020-10-18 19:53:21 +0200

    Added myobject/

---
+++ myobject/object.py
@@ -0,0 +1,52 @@
+from numpy import clip, inner
+from numpy.linalg import norm
+
+from core.ray import Ray
+from core.tracer import get_nearest_obstacle
+
+def normalize(vector):
+    return vector / norm(vector)
+
+class Object:
+    max_recursion_depth = 5
+    counter = 0
+
+    def __init__(self, properties):
+        self.properties = properties
+        self.id = Object.counter
+        Object.counter += 1
+
+    def intersect(self, ray):
+        pass
+
+    def get_normal(self, p):
+        pass
+
+    def transform(self, matrix):
+        pass
+
+    def shader(self, p, d, lightsource, object_list, recursion_depth=1):
+        normal = self.get_normal(p)
+
+        # ambient
+        color = self.properties.ambient * self.properties.color
+
+        # diffuse
+        if self.properties.diffuse > 0.0:
+            lightray = Ray(lightsource, p - lightsource)
+            nearest_obstacle, t = get_nearest_obstacle(lightray, object_list)
+            if nearest_obstacle is not None and nearest_obstacle.id == self.id:
+                diffuse_coefficient = -inner(normal, normalize(lightray.direction))
+                diffuse_coefficient = clip(diffuse_coefficient, 0.0, 1.0)
+                color += self.properties.diffuse * diffuse_coefficient * self.properties.color
+
+        # reflection
+        if self.properties.reflection > 0.0 and Object.max_recursion_depth > recursion_depth:
+            reflection_ray = Ray(p, d - 2.0 * inner(d, normal) * normal)
+            obj, t = get_nearest_obstacle(reflection_ray, object_list)
+            if obj is not None:
+                reflection_color = obj.shader(reflection_ray(t), reflection_ray.direction, \
+                lightsource, object_list, recursion_depth - 1)
+                color += self.properties.reflection * reflection_color
+
+        return color
---
+++ myobject/sphere.py
@@ -0,0 +1,38 @@
+from numpy import array, inf, inner, sqrt
+
+from object.object import Object, normalize
+
+
+class Sphere(Object):
+    def __init__(self, m, r, properties):
+        super().__init__(properties)
+        self.m = m
+        self.r = r
+
+    #---BEGIN intersect---
+    def intersect(self, ray):
+        v = ray.origin
+        w = ray.direction
+
+        """
+        Ersetzen Sie diesen Kommentar durch Ihren Code:
+        Gegeben ist ein Strahl mit Ursprung v und Richtung w.
+        Geben Sie den Parameter t > 0 zurück, so dass v + t * w
+        gerade der an v nächstgelegene Schnittpunkt des Strahls
+        mit der Kugel ist. Falls dieser nicht existiert soll
+        t = inf (also Unendlich) zurückgegeben werden. 
+        """
+
+        return inf
+    #---END intersect---
+
+    #---BEGIN get_normal---
+    def get_normal(self, p):
+        """
+        Ersetzen Sie diesen Kommentar durch Ihren Code:
+        Sei p ein Punkt auf der Kugeloberfläche.
+        Geben Sie den nach aussen zeigenden, normierten
+        Normalenvektor auf der Kugel am Punkt p zurück.
+        """
+        return array([0.0, 0.0, 0.0])
+    #---BEGIN get_normal---
