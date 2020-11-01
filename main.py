de7103a20acd0d8d76e8cbea35ce48d6b2f24550
Oliver Rietmann <rioliver@student.ethz.ch>
2020-11-01 13:55:48 +0100

    objects --> object_list

--- main.py
+++ main.py
@@ -12,26 +12,26 @@ renderer = Renderer(camera)
 
 lightsource_list = [array([0.0, 5.0, 10.0]), array([-5.0, 10.0, 5.0])]
 
-objects = []
+object_list = []
 
 red = array([1.0, 0.0, 0.0])
 properties = Properties(color=red, ambient=0.2, diffuse=0.8)
-objects.append(Sphere(array([4.0, -2.0, 0.0]), 1.0, properties))
+object_list.append(Sphere(array([4.0, -2.0, 0.0]), 1.0, properties))
 
 
 purple = array([148.0 / 255.0, 0.0, 211.0 / 255.0])
 properties = Properties(color=purple, ambient=0.2, diffuse=0.2, phong=[1.0, 30], reflection=0.6)
-objects.append(Sphere(array([6.0, 2.0, 1.0]), 2.0, properties))
+object_list.append(Sphere(array([6.0, 2.0, 1.0]), 2.0, properties))
 
 
 blue = array([0.0, 0.0, 1.0])
 properties = Properties(color=blue, ambient=0.2, diffuse=0.8)
-objects.append(Box(array([-0.5, -3.0, -1.0]), array([0.5, -2.0, 0.0]), properties))
+object_list.append(Box(array([-0.5, -3.0, -1.0]), array([0.5, -2.0, 0.0]), properties))
 
 
 gray = array([0.7, 0.7, 0.7])
 properties = Properties(color=gray, ambient=0.2, diffuse=0.6, phong=[0.0, 0], reflection=0.2)
-objects.append(Plane(array([0.0, 0.0, 1.0]), 1.0, properties))
+object_list.append(Plane(array([0.0, 0.0, 1.0]), 1.0, properties))
 
-renderer(lightsource_list, objects)
+renderer(lightsource_list, object_list)
 renderer.save_image("image.png")
